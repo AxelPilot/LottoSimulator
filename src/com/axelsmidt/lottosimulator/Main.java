b2470658646fea4e7abf84f92d917d6ea1749f2b
Axel Smidt <axelpilot78@gmail.com>
2020-07-19 19:43:16 +0200

    Bug fixes and improvements.

--- src/com/axelsmidt/lottosimulator/Main.java
+++ src/com/axelsmidt/lottosimulator/Main.java
@@ -23,7 +23,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        int playAgain = 0;
+        int playAgain;
         Scanner input = new Scanner(System.in);
 
         do {
